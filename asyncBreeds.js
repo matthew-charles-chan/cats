ed1606bde804df8c73ec18a81906553413c5bf87
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-15 02:12:21 +0000

    mocha chai testing on cats

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -8,15 +8,22 @@ const breedDetailsFromFile = function(breed, cbFunction) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
   
     // 3. once data is pulled, callback function is called
-    if (!error) cbFunction(data)
+    if (!error) {
+      cbFunction(data)
+    } else {
+      cbFunction(undefined)
+    }
   });
 };
 
 // 4. callback function called and data from 'breedDetailsFromFile' function is printed.
 const printBreedData = function (breed) {
-  console.log(breed)
+  // console.log(breed)
 }
 
 
 // 1. call 'breedDetailsFromFile' function: takes in 'breed' and callbackFunction: printBreedData
-breedDetailsFromFile('Bombay', printBreedData);
+const bombay = breedDetailsFromFile('Bombay', printBreedData);
+// const balinese = breedDetailsFromFile('Balinese', printBreedData);
+
+module.exports = breedDetailsFromFile
